03debef524c2b4de81d9c014c1c52d19edc4f872
Patrick Lin <patrick@tensoropera.com>
2024-05-30 12:44:50 -0700

    addressed all errors

--- setup/install.sh
+++ setup/install.sh
@@ -15,20 +15,22 @@ sudo chown sshpiper:sshpiper /home/sshpiper
 # install files
 ARCH=$(dpkg --print-architecture)
 if [ "$ARCH" = "amd64" ]; then
-    sudo wget https://github.com/TensorOpera-Inc/sshproxy/releases/download/v1.3.0/sshpiper_x86_64.tar.gz -O /home/sshpiper/download.tar.gz
+    sudo wget https://github.com/TensorOpera-Inc/sshproxy/releases/download/v1.3.1/sshpiper_x86_64.tar.gz -O /home/sshpiper/download.tar.gz
 elif [ "$ARCH" = "arm64" ]; then
-    sudo wget https://github.com/TensorOpera-Inc/sshproxy/releases/download/v1.3.0/sshpiper_aarch64.tar.gz -O /home/sshpiper/download.tar.gz
+    sudo wget https://github.com/TensorOpera-Inc/sshproxy/releases/download/v1.3.1/sshpiper_aarch64.tar.gz -O /home/sshpiper/download.tar.gz
 else
     echo "Unsupported architecture: $ARCH, skipping installation of SSH proxy"
     exit 1
 fi
-sudo tar -xvzf /home/sshpiper/download.tar.gz
+sudo tar -xvzf /home/sshpiper/download.tar.gz -C /home/sshpiper
 # reorganize files here or something
 sudo curl https://raw.githubusercontent.com/TensorOpera-Inc/sshproxy/master/setup/initial.yaml -o /home/sshpiper/config.yaml
-sudo ssh-keygen -t ed25519 -N "" -f /home/sshpiper/sshpiper_ed25519
+sudo ssh-keygen -t ed25519 -N "" -f /home/sshpiper/sshpiper_ed25519 -C "TensorOpera_Agent"
+sudo mkdir /home/sshpiper/keys
 sudo chown -R sshpiper:sshpiper /home/sshpiper/*
-sudo chmod -R 500 /home/sshpiper/sshpiperd /home/sshpiper/plugins
+sudo chmod 500 /home/sshpiper/sshpiperd /home/sshpiper/yaml
 sudo chmod 600 /home/sshpiper/config.yaml
+sudo chmod 700 /home/sshpiper/keys
 sudo chmod 400 /home/sshpiper/sshpiper_ed25519
 sudo chmod 404 /home/sshpiper/sshpiper_ed25519.pub
 # install service
@@ -42,9 +44,10 @@ sudo rm /home/sshpiper/download.tar.gz
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo apt-get install -y iptables-persistent
-sudo iptables -A INPUT -p tcp --dport 2222 -m iprange --src-range 172.16.0.0-172.31.255.255 -j DROP
-sudo iptables -A INPUT -p tcp --dport 2222 -m iprange --src-range 10.0.0.0-10.255.255.255 -j DROP
-sudo iptables -A INPUT -p tcp --dport 2222 -m iprange --src-range 192.168.0.0-192.168.255.255 -j DROP
+sudo iptables -A INPUT -p tcp --dport 2222 -m iprange --src-range 172.16.0.0-172.31.255.255 -j REJECT
+sudo iptables -A INPUT -p tcp --dport 2222 -m iprange --src-range 10.0.0.0-10.255.255.255 -j REJECT
+sudo iptables -A INPUT -p tcp --dport 2222 -m iprange --src-range 192.168.0.0-192.168.255.255 -j REJECT
+sudo iptables -I INPUT -p tcp --dport 2222 -i lo -j REJECT
 sudo iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
 sudo systemctl is-active --quiet netfilter-persistent || sudo systemctl start netfilter-persistent
 sudo netfilter-persistent save
@@ -58,5 +61,6 @@ exit 0
 # ├── sshpiper_ed25519
 # ├── sshpiper_ed25519.pub
 # ├── sshpiper
-# └── yaml
+# ├── yaml
+# └── keys
 # /etc/systemd/system/sshpiper.service
